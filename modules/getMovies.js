a1b84900752714b00efb23a438624d9a8a9665c7
Julian Gallegos <julianjgallegos1@gmail.com>
2022-09-30 23:12:07 +0000

    Cache implemented for back-end

--- modules/getMovies.js
+++ modules/getMovies.js
@@ -1,21 +1,32 @@
 'use strict';
 
 const axios = require('axios');
+let cache = require('./cache');
 
 async function getMovies(req, res) {
     const searchQuery = req.query.query;
+    const key = 'movie-' + searchQuery;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
-    try {
-        const movieResponse = await axios.get(url);
-        let moviesArray = [];
-        for (let i = 0; i < movieResponse.data.results.length || i < 20; i++) {
-            let result = movieResponse.data.results[i];
-            moviesArray.push(new Movie(result.original_title, result.overview, result.vote_average, result.vote_count, 'https://image.tmdb.org/t/p/w500'+result.poster_path, result.popularity, result.release_date));
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+        console.log('Cache hit');
+    } else {
+        try {
+            const movieResponse = await axios.get(url);
+            let moviesArray = [];
+            for (let i = 0; i < movieResponse.data.results.length || i < 20; i++) {
+                let result = movieResponse.data.results[i];
+                moviesArray.push(new Movie(result.original_title, result.overview, result.vote_average, result.vote_count, 'https://image.tmdb.org/t/p/w500' + result.poster_path, result.popularity, result.release_date));
+            }
+            cache[key] = {};
+            cache[key].timestamp = Date.now();
+            cache[key].data = moviesArray;
+        } catch (error) {
+            res.status(500).send(`server error ${error}`);
         }
-        res.status(200).send(moviesArray);
-    } catch (error) {
-        res.status(500).send(`server error ${error}`);
     }
+
+    res.status(200).send(cache[key].data);
 }
 
 class Movie {
--- modules/getWeather.js
+++ modules/getWeather.js
@@ -1,26 +1,37 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('./cache');
 
 async function getWeather(req, res) {
     //const searchQuery = req.query.query;
     const lat = req.query.lat;
     const lon = req.query.lon;
+    const key = 'weather-'+lat+lon;
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
-    try {
-        const weatherResponse = await axios.get(url);
-        let forecastArray = weatherResponse.data.data.map(obj => {
-            let date = obj.datetime;
-            let low = obj.min_temp;
-            let high = obj.max_temp;
-            let weather = obj.weather.description;
-            let description = `Low of ${low}, high of ${high} with ${weather}`;
-            return new Forecast(date, description);
-        });
-        res.status(200).send(forecastArray);
-    } catch (error) {
-        res.status(500).send(`server error ${error}`)
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+        console.log('Cache hit');
+    } else {
+        try {
+            const weatherResponse = await axios.get(url);
+            let forecastArray = weatherResponse.data.data.map(obj => {
+                let date = obj.datetime;
+                let low = obj.min_temp;
+                let high = obj.max_temp;
+                let weather = obj.weather.description;
+                let description = `Low of ${low}, high of ${high} with ${weather}`;
+                return new Forecast(date, description);
+            });
+            cache[key] = {};
+            cache[key].timestamp = Date.now();
+            cache[key].data = forecastArray;
+        } catch (error) {
+            res.status(500).send(`server error ${error}`)
+        }
     }
+
+    res.status(200).send(cache[key].data);
 }
 
 class Forecast {
